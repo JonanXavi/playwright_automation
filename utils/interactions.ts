399fd30deb35698618b5b0fd471d3f41205c174b
jonanxavi <jonathanfer121@gmail.com>
2025-05-19 21:44:49 -0500

    code refactor & .env.example file

--- utils/interactions.ts
+++ utils/interactions.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page} from '@playwright/test';
+import { type Page} from '@playwright/test';
 
 export const clickOnButton = async (page: Page, button: string) => {
     await page.getByRole('button', { name: button }).click();
