ec2fe7ec77d1a1716f9448d6cd6dfab4ce00af36
jonanxavi <61487582+JonanXavi@users.noreply.github.com>
2025-10-26 18:53:24 -0500

    add linters && code correction

--- utils/env.ts
+++ utils/env.ts
@@ -6,7 +6,7 @@ config({
 });
 
 export class ENV {
-    public static readonly BASE_URL = process.env.URL;
-    public static readonly USER = process.env.USER;
-    public static readonly PASSWORD = process.env.PASSWORD;
+    public static readonly BASE_URL = process.env.URL ?? '';
+    public static readonly USER = process.env.USER ?? '';
+    public static readonly PASSWORD = process.env.PASSWORD ?? '';
 }
